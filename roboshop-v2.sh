83f5d91fa908afb8a394c7d489179072db51ee0e
pavanisai2828 <pavani.sai1313@gmailcom>
2026-07-02 04:25:17 +0530

    second code added

---
+++ roboshop-v2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z07423591GD2G8GTI4KPQ"
+DOMAIN_NAME="daws-90.online"
+
+#########Validation##########
+if [ $# -lt 2 ]; then
+    echo "atleast 2 aruguemts need to be passed"
+    exit 1
+fi 
+
