09aa64ebdef4a96d54ac18ae323b0de20e7d442c
pavanisai2828 <pavani.sai1313@gmailcom>
2026-07-02 04:53:32 +0530

    second code added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -116,7 +116,7 @@ do
         fi
 
     else
-        if [ "$INSTANCE_ID" == "None" ];
+        if [ "$INSTANCE_ID" == "None" ];then
             echo "$instance already destroyed, nothing to do..."
         else
             aws ec2 terminate-instances --instance-ids $INSTANCE_ID
